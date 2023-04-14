e2a8c219654dd817f155de61f6e923c8ba4488ac
Aleksandr Sabirov <sabirov.aleksandr@gmail.com>
2023-04-14 01:18:06 -0400

    Отправка на первый этап ревью

---
+++ backend/api/mixins.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from core.enums import Tuples
+from django.db.models import Model, Q
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
+from rest_framework.status import (HTTP_201_CREATED, HTTP_204_NO_CONTENT,
+                                   HTTP_400_BAD_REQUEST)
+
+
+class AddDelViewMixin:
+    """
+    Добавляет во Viewset дополнительные методы.
+    Содержит метод добавляющий/удаляющий объект связи
+    Many-to-Many между моделями.
+    Требует определения атрибута `add_serializer`.
+    """
+
+    add_serializer: ModelSerializer | None = None
+
+    def _add_del_obj(
+        self,
+        obj_id: int | str,
+        m2m_model: Model,
+        q: Q
+    ) -> Response:
+        """Добавляет/удаляет связь M2M между пользователем и другим объектом.
+        """
+        obj = get_object_or_404(self.queryset, id=obj_id)
+        serializer: ModelSerializer = self.add_serializer(obj)
+        m2m_obj = m2m_model.objects.filter(q & Q(user=self.request.user))
+
+        if (self.request.method in Tuples.ADD_METHODS) and not m2m_obj:
+            m2m_model(None, obj.id, self.request.user.id).save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+
+        if (self.request.method in Tuples.DEL_METHODS) and m2m_obj:
+            m2m_obj[0].delete()
+            return Response(status=HTTP_204_NO_CONTENT)
+
+        return Response(status=HTTP_400_BAD_REQUEST)
---
+++ backend/api/urls.py
@@ -0,0 +1,24 @@
+from api.views import (BaseAPIRootView, IngredientViewSet, RecipeViewSet,
+                       TagViewSet, UserViewSet)
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+app_name = 'api'
+
+
+class RuDefaultRouter(DefaultRouter):
+    """Показывает описание главной страницы API на русском языке.
+    """
+    APIRootView = BaseAPIRootView
+
+
+v1_router = RuDefaultRouter()
+v1_router.register('tags', TagViewSet, 'tags')
+v1_router.register('ingredients', IngredientViewSet, 'ingredients')
+v1_router.register('recipes', RecipeViewSet, 'recipes')
+v1_router.register('users', UserViewSet, 'users')
+
+urlpatterns = (
+    path('v1/', include(v1_router.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
+)
